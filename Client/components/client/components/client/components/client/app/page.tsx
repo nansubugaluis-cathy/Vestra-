f783deaadffdb9765c427b704662b61c75707ea8
nansubugaluis-cathy <nansubugaluis@gmail.com>
2026-07-12 06:49:04 +0300

    Add Home component with multiple sections

---
+++ Client/components/client/components/client/components/client/app/page.tsx
@@ -0,0 +1,39 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import BrandIntroduction from "@/components/BrandIntroduction";
+import OurPromise from "@/components/OurPromise";
+import WhyChoose from "@/components/WhyChoose";
+import FeaturedProducts from "@/components/FeaturedProducts";
+import Vision from "@/components/Vision";
+import Footer from "@/components/Footer";
+import WhatsAppChat from "@/components/WhatsAppChat";
+
+export default function Home() {
+
+return (
+
+<>
+
+<Navbar />
+
+<Hero />
+
+<BrandIntroduction />
+
+<OurPromise />
+
+<WhyChoose />
+
+<FeaturedProducts />
+
+<Vision />
+
+<Footer />
+
+<WhatsAppChat />
+
+</>
+
+)
+
+}
